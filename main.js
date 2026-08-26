403f452c5de46d3e041f2f9035bf00d5f250bfcf
kosar8651 <kosar1386motaqi@gmail.com>
2026-08-26 10:27:44 +0330

    sum

---
+++ main.js
@@ -0,0 +1,7 @@
+x = + prompt("number")
+y = + prompt("number")
+document.getElementById("sum").innerHTML=`<h1> sum:${ x + y} </h1>`
+document.getElementById("sub").innerHTML=`<h1> sub:${ x - y} </h1>`
+document.getElementById("div").innerHTML=`<h1> div:${ x / y} </h1>`
+document.getElementById("mul").innerHTML=`<h1> mul:${ x * y} </h1>`
+document.getElementById("pow").innerHTML=`<h1> pow:${ x ** y} </h1>`
